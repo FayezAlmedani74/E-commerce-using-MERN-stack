2918943a707de09dad90c95ff5e0c83771c6e417
FayezAl-medani <fayezalmedani7@gmail.com>
2024-08-01 23:36:54 +0300

    AuthContext + Register Page Integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
+import AuthProvider from "./context/Auth/AuthProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/user/register" element={<RegisterPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/user/register" element={<RegisterPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import { BASE_URL } from "../constants/baseUrl";
+import { useAuth } from "../context/Auth/AuthContext";
 
 const RegisterPage = () => {
   const [error, setError] = useState("");
@@ -8,12 +9,17 @@ const RegisterPage = () => {
   const lastNameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const { login } = useAuth();
 
   const onSubmit = async () => {
     const firstName = firstNameRef.current?.value;
     const lastName = lastNameRef.current?.value;
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
+    if (!firstName || !lastName || !email || !password) {
+      setError("Check submitted data!");
+      return;
+    }
     console.log(firstName, lastName, email, password);
     const response = await fetch(`${BASE_URL}/user/register`, {
       method: "POST",
@@ -31,8 +37,14 @@ const RegisterPage = () => {
       setError("Enable to register user, please try different credientials!");
       return;
     }
-    const data = await response.json();
-    console.log(data);
+    const token = await response.json();
+    if (!token) {
+      setError("Incorrect token");
+      return;
+    }
+
+    login(email, token);
+    console.log(token);
   };
   return (
     <Container>
