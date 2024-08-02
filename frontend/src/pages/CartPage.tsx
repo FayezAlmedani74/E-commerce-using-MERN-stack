087a34a75c6178a1afa2c690d1d017300486bd64
FayezAl-medani <fayezalmedani7@gmail.com>
2024-08-02 18:18:52 +0300

    Cart Page layout

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,21 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Box, Container, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import { useCart } from "../context/Cart/CartContext";
 
 const CartPage = () => {
-  const { token } = useAuth();
   const { cartItems, totalAmount } = useCart();
-  const [, setError] = useState("");
 
   return (
     <Container sx={{ mt: 2 }}>
       <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item: { title: any }) => (
-        <Box>{item.title}</Box>
-      ))}
+      <Box display={"flex"} flexDirection={"column"} gap={4}>
+        {cartItems.map((item) => (
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            sx={{
+              border: 1,
+              borderColor: "#f2f2f2",
+              borderRadius: 5,
+              padding: 1,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: " row",
+                alignItems: "center",
+                gap: 1,
+              }}
+            >
+              <img src={item.image} alt="" width={100} />
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} * {item.unitPrice} SYR
+                </Typography>
+                <Button>RemoveItem</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant="h4">Total Amount: {totalAmount} SYR</Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
